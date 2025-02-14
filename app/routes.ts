23d987972bd9a0b13fedac4fbc64acc465dc7cc6
yupix <yupi0982@outlook.jp>
2025-02-14 11:57:00 +0900

    feat: remix flat routesを使うように

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,9 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+// app/routes.ts
+import { remixRoutesOptionAdapter } from '@react-router/remix-routes-option-adapter';
+import { flatRoutes } from 'remix-flat-routes';
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export const routes = remixRoutesOptionAdapter((defineRoutes) => {
+	return flatRoutes('routes', defineRoutes, {
+		ignoredRouteFiles: ['**/.*'], // Ignore dot files (like .DS_Store)
+	});
+});
