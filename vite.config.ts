61dcb2fea9a305fc5faf8636e5a121c73e0ac891
yupix <yupi0982@outlook.jp>
2025-02-14 11:48:28 +0900

    feat: PORTを.envで変更できるように

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, loadEnv } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+
+	return {
+		plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+		server: {
+			port: Number.parseInt(env.PORT || '5173'),
+		},
+	};
 });
