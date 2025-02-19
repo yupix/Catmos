e47f0c2b0ef4ae3b4fea2eb00850c05c855886c0
yupix <yupi0982@outlook.jp>
2025-02-19 18:50:58 +0900

    chore: ログインボタンを追加

--- app/routes/welcome.tsx
+++ app/routes/welcome.tsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router';
+import { Button } from '~/components/shadcn/ui/button';
+
 export default function Index() {
 	return (
 		<div className="flex h-screen w-screen items-center">
 			<div>
 				<h1>CatMos</h1>
 				<p>CatMosは誰でも簡単に使用することができるSNSです。</p>
+
+				<Button asChild>
+					<Link to="/auth/login">ログイン</Link>
+				</Button>
 			</div>
 		</div>
 	);
