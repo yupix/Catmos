4d801cbc5ff5d947dc7d01c8dd1e0997995b6162
yupix <yupi0982@outlook.jp>
2025-02-24 20:28:10 +0900

    feat: 設定ルートとプロフィールアップロード機能を追加

---
+++ app/routes/settings+/_route.tsx
@@ -0,0 +1,20 @@
+import { Outlet, redirect } from 'react-router';
+import type { Route } from '../+types';
+
+export function loader({ request }: Route.LoaderArgs) {
+	const url = new URL(request.url);
+
+	// レイアウト用のルートなので、/settingsにアクセスされたら/profileにリダイレクト
+	if (url.pathname === '/settings') {
+		return redirect('/settings/profile', { status: 302 });
+	}
+	return null;
+}
+
+export default function Layout() {
+	return (
+		<div className="bg-red-300">
+			<Outlet />
+		</div>
+	);
+}
