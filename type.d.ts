72ffa3ecf8dbc770a1eea978d479f9c79ec7ed9d
yupix <yupi0982@outlook.jp>
2025-03-02 18:05:22 +0900

    refactor: 不要なインポートを削除し、型定義を整理

--- type.d.ts
+++ type.d.ts
@@ -1,11 +1,4 @@
 /// <reference types="vite/client" />
-
-import type { FC } from 'react';
-
 interface ImportMetaEnv {
 	VITE_ORIGIN: string;
-}
-
-declare module 'react' {
-	type FCX<P = Record<string, unknown>> = FC<P & { className?: string }>;
 }
