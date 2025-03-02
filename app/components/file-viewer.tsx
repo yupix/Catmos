0e3832141c4ae56d6989526015e0744f62a4398e
yupix <yupi0982@outlook.jp>
2025-03-02 20:30:33 +0900

    chore: ファイルビュワーのスタイルを調整

--- app/components/file-viewer.tsx
+++ app/components/file-viewer.tsx
@@ -7,6 +7,11 @@ interface FileViewerProps {
 	isCompact?: boolean;
 }
 
+/**
+ * ファイルを表示するコンポーネント
+ * @param {FileViewerProps} props - ファイルと表示オプション
+ * @returns {JSX.Element} ファイル表示コンポーネント
+ */
 export const FileViewer: FCX<FileViewerProps> = ({
 	file,
 	className,
@@ -19,9 +24,9 @@ export const FileViewer: FCX<FileViewerProps> = ({
 					src={file.url}
 					alt={file.filename}
 					className={cn(
-						'object-contain w-20 ',
+						'object-contain aspect-video',
+						!isCompact && 'xs:w-[calc(50%-8px)]',
 						className,
-						!isCompact && 'xs:w-[calc(50%-8px)] md:aspect-video',
 					)}
 					style={{
 						background: !isCompact
@@ -38,7 +43,7 @@ export const FileViewer: FCX<FileViewerProps> = ({
 					src={file.url}
 					controls
 					className={cn(
-						'mb-2 h-full max-h-[400px] @min-[500px]:w-[calc(50%-8px)] rounded-sm object-contain object-center md:mb-0',
+						'object-contain aspect-video mb-2 h-full max-h-[400px] @min-[500px]:w-[calc(50%-8px)] rounded-sm object-center md:mb-0',
 						className,
 					)}
 				/>
