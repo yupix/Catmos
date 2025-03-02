8b8f0f596e02669fbbfa867b92465805524084ff
yupix <yupi0982@outlook.jp>
2025-03-02 18:05:13 +0900

    feat: useMemoを使用してパフォーマンスを向上させる

--- app/components/timeline.tsx
+++ app/components/timeline.tsx
@@ -1,5 +1,5 @@
 import { AnimatePresence, motion } from 'motion/react';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { ClientOnly } from 'remix-utils/client-only';
 import SuperJSON from 'superjson';
 import { useScrollPosition } from '~/hooks/use-scroll';
@@ -74,37 +74,40 @@ export function Client({ initMeows }: TimelineProps) {
 		};
 	}, [isAtTop, newMeows]);
 
-	return (
-		<div className="w-full p-[1px] md:p-5">
-			{newMeows.length > 0 && (
-				<button
-					type="button"
-					className="sticky top-10 mx-auto mb-4 block w-fit cursor-pointer rounded-xl bg-blue-400 px-5 py-2 text-center text-sm text-white transition-all duration-200 hover:bg-blue-500"
-					onClick={handleScrollToTop}
-					onKeyDown={handleScrollToTop}
-				>
-					{newMeows.length} 件の鳴き声があります
-				</button>
-			)}
-			{isConnected ? (
-				<AnimatePresence>
-					{meows.map((meow) => (
-						<motion.div
-							className="my-6 border-b pb-6"
-							key={meow.id}
-							layout
-							initial={{ opacity: 0, y: -20, scale: 0.9 }}
-							animate={{ opacity: 1, y: 0, scale: 1 }}
-							exit={{ opacity: 0, y: -20, scale: 0.9 }}
-							transition={{ duration: 0.4, ease: 'easeOut' }}
-						>
-							<Meow meow={meow} />
-						</motion.div>
-					))}
-				</AnimatePresence>
-			) : (
-				<div>Connecting...</div>
-			)}
-		</div>
+	return useMemo(
+		() => (
+			<div className="w-full p-[1px] md:p-5">
+				{newMeows.length > 0 && (
+					<button
+						type="button"
+						className="sticky top-10 mx-auto mb-4 block w-fit cursor-pointer rounded-xl bg-blue-400 px-5 py-2 text-center text-sm text-white transition-all duration-200 hover:bg-blue-500"
+						onClick={handleScrollToTop}
+						onKeyDown={handleScrollToTop}
+					>
+						{newMeows.length} 件の鳴き声があります
+					</button>
+				)}
+				{isConnected ? (
+					<AnimatePresence>
+						{meows.map((meow) => (
+							<motion.div
+								className="my-6 border-b pb-6"
+								key={meow.id}
+								layout
+								initial={{ opacity: 0, y: -20, scale: 0.9 }}
+								animate={{ opacity: 1, y: 0, scale: 1 }}
+								exit={{ opacity: 0, y: -20, scale: 0.9 }}
+								transition={{ duration: 0.4, ease: 'easeOut' }}
+							>
+								<Meow meow={meow} />
+							</motion.div>
+						))}
+					</AnimatePresence>
+				) : (
+					<div>Connecting...</div>
+				)}
+			</div>
+		),
+		[isConnected, isAtTop, newMeows, meows],
 	);
 }
