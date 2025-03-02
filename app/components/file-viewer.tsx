f3650bc2e4c7cfe912e26c0bc674884ab11a2dcd
yupix <yupi0982@outlook.jp>
2025-03-02 20:47:17 +0900

    fix: react-photo-viewが動かない close #29

--- app/components/file-viewer.tsx
+++ app/components/file-viewer.tsx
@@ -1,10 +1,13 @@
 import type { File as FileModel } from '@prisma/client';
-import type { FCX } from 'react';
+import type { FC } from 'react';
 import { cn } from '~/lib/utils';
 
 interface FileViewerProps {
 	file: FileModel;
 	isCompact?: boolean;
+	className: string;
+	onClick?: () => void;
+	ref?: React.Ref<HTMLImageElement | HTMLVideoElement>;
 }
 
 /**
@@ -12,10 +15,12 @@ interface FileViewerProps {
  * @param {FileViewerProps} props - ファイルと表示オプション
  * @returns {JSX.Element} ファイル表示コンポーネント
  */
-export const FileViewer: FCX<FileViewerProps> = ({
+export const FileViewer: FC<FileViewerProps> = ({
 	file,
 	className,
 	isCompact,
+	onClick,
+	ref,
 }) => {
 	switch (file.mimetype.split('/')[0]) {
 		case 'image':
@@ -33,6 +38,9 @@ export const FileViewer: FCX<FileViewerProps> = ({
 							? 'repeating-linear-gradient(45deg, #f3f4f6, #f3f4f6 10px, #e5e7eb 10px, #e5e7eb 20px)'
 							: undefined,
 					}}
+					onClick={onClick}
+					onKeyDown={onClick}
+					ref={ref as React.Ref<HTMLImageElement>}
 				/>
 			);
 		case 'video':
@@ -46,6 +54,9 @@ export const FileViewer: FCX<FileViewerProps> = ({
 						'object-contain aspect-video mb-2 h-full max-h-[400px] @min-[500px]:w-[calc(50%-8px)] rounded-sm object-center md:mb-0',
 						className,
 					)}
+					onClick={onClick}
+					onKeyDown={onClick}
+					ref={ref as React.Ref<HTMLVideoElement>}
 				/>
 			);
 		default:
