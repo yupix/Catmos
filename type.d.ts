319502e2b06540d757e33b45252d25bb6908f383
yupix <yupi0982@outlook.jp>
2025-03-02 18:48:26 +0900

    feat: react-scriptsの型定義を追加し、FCX型を定義

--- type.d.ts
+++ type.d.ts
@@ -2,3 +2,8 @@
 interface ImportMetaEnv {
 	VITE_ORIGIN: string;
 }
+
+/// <reference types="react-scripts" />
+declare module 'react' {
+  type FCX<P = {}> = React.FC<P & { className?: string }>
+}
