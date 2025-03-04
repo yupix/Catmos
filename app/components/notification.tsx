c6e27e2029fd58891b6d9efdb83f03749c603b30
yupix <yupi0982@outlook.jp>
2025-03-05 01:04:50 +0900

    feat: 通知をredis上に構築

--- app/components/notification.tsx
+++ app/components/notification.tsx
@@ -3,8 +3,12 @@ import type {
 	Meow as MeowModel,
 	User,
 } from '@prisma/client';
-import { TbBell } from 'react-icons/tb';
+import { TbBell, TbPlus } from 'react-icons/tb';
+import type { BaseNotification } from '~/lib/notification.server';
+import { getUserName } from '~/lib/utils';
+import { HoverUserCard } from './hover-user-card';
 import { Meow } from './meow';
+import { TimeDisplay } from './meow/time';
 import { Avatar, AvatarFallback, AvatarImage } from './shadcn/ui/avatar';
 
 interface NotificationProps {
@@ -13,7 +17,7 @@ interface NotificationProps {
 
 export interface INotification {
 	id: string;
-	type: 'reaction' | 'reply' | 'mention' | 'remeow';
+	type: BaseNotification['type'];
 	meow?: MeowModel & {
 		author: User;
 		attachments: FileModel[];
@@ -31,11 +35,13 @@ export interface INotification {
 }
 
 export function Notification({ notification }: NotificationProps) {
-	const notificationIcon = (type: INotification['type']) => {
+	const notificationIcon = (type: BaseNotification['type']) => {
 		switch (type) {
 			case 'reaction':
 				return <TbBell className="h-5 w-5" />;
-			case 'reply':
+			case 'follow':
+				return <TbPlus className="h-5 w-5 text-blue-500" />;
+			case 'mention':
 				return <TbBell className="h-5 w-5" />;
 			default:
 				return <TbBell className="h-5 w-5" />;
@@ -49,43 +55,59 @@ export function Notification({ notification }: NotificationProps) {
 					<Meow meow={notification.meow} size="xs" isCompactFile />
 				</div>
 			) : (
-				<div className="mb-2 flex w-full items-center gap-2 p-2">
-					<div className="relative shrink-0">
-						<Avatar className="h-12 w-12">
-							<AvatarImage
-								src={notification.user.avatarUrl}
-								alt="avatar"
-								className=""
-							/>
-							<AvatarFallback>{notification.user.name}</AvatarFallback>
-						</Avatar>
-						<div className="-bottom-1 -right-1 absolute rounded-full bg-white p-0.5">
-							{notificationIcon(notification.type)}
+				<div className="mb-2 flex w-full items-center gap-2 px-4 py-2">
+					<div className="flex w-full items-center gap-2">
+						<div className="relative shrink-0">
+							<Avatar className="h-12 w-12">
+								<AvatarImage
+									src={notification.user.avatarUrl}
+									alt="avatar"
+									className=""
+								/>
+								<AvatarFallback>{notification.user.name}</AvatarFallback>
+							</Avatar>
+							<div className="-bottom-1 -right-1 absolute rounded-full bg-white p-0.5">
+								{notificationIcon(notification.type)}
+							</div>
 						</div>
-					</div>
 
-					<div className="flex w-full flex-col gap-2">
-						<span className="text-sm">
-							{notification.type === 'reaction' && (
-								<>
-									<span className="font-bold text-blue-500">
-										{notification.user.name}
-									</span>
-									<span>さんがあなたの鳴き声にリアクションしました</span>
-								</>
-							)}
-							{notification.type === 'remeow' && (
-								<>
-									<span className="font-bold text-blue-500">
-										{notification.user.name}
-									</span>
-									<span>さんがあなたの鳴き声をリメウしました</span>
-								</>
-							)}
-						</span>
-						<span className="text-xs text-gray-500">
-							{notification.createdAt.toLocaleString()}
-						</span>
+						<div className="flex w-full flex-col gap-0 text-xs ">
+							<div className="w-full overflow-hidden">
+								<div className="flex justify-between">
+									<HoverUserCard
+										user={notification.user}
+										className="shrink-999999 overflow-hidden overflow-ellipsis whitespace-nowrap"
+									>
+										{getUserName(notification.user, true)}
+									</HoverUserCard>
+
+									<div className="shrink-0">
+										<TimeDisplay date={notification.createdAt} />
+									</div>
+								</div>
+								<p className="text-slate-600 text-xs">
+									{notification.type === 'follow' && (
+										<span>フォローされました</span>
+									)}
+									{notification.type === 'reaction' && (
+										<>
+											<span className="font-bold text-blue-500">
+												{notification.user.name}
+											</span>
+											<span>さんがあなたの鳴き声にリアクションしました</span>
+										</>
+									)}
+									{notification.type === 'remeow' && (
+										<>
+											<span className="font-bold text-blue-500">
+												{notification.user.name}
+											</span>
+											<span>さんがあなたの鳴き声をリメウしました</span>
+										</>
+									)}
+								</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			)}
