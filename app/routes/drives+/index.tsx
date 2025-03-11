f488d0f9ad5d2272a714ffa81350d0bae099a073
yupix <yupi0982@outlook.jp>
2025-03-11 22:45:12 +0900

    feat: ファイルがまだ無いときにテキストと画像を表示

--- app/routes/drives+/index.tsx
+++ app/routes/drives+/index.tsx
@@ -1,5 +1,6 @@
 import { Link, useLoaderData } from 'react-router';
 import { FileViewer } from '~/components/file-viewer';
+import { SpecificImage } from '~/components/specific-image';
 import { getUserSession } from '~/lib/auth/auth.server';
 import { prisma } from '~/lib/db';
 import type { Route } from './+types';
@@ -23,20 +24,27 @@ export default function Index() {
 	const { files } = useLoaderData<typeof loader>();
 	return (
 		<div className=" flex flex-wrap justify-center gap-8 bg-slate-50">
-			{files.map((file) => (
-				<Link
-					to={`/drives/file/${file.id}`}
-					key={file.id}
-					className="@container mb-2 flex w-full max-w-28 shrink-0 cursor-pointer flex-col items-center gap-2 rounded-xl p-4 transition-all duration-75 hover:bg-slate-100"
-				>
-					<FileViewer
-						file={file}
-						className="aspect-square shrink-0 bg-white"
-						isCompact
-					/>
-					<p className="break-all text-gray-500 text-xs">{file.filename}</p>
-				</Link>
-			))}
+			{files.length > 0 ? (
+				files.map((file) => (
+					<Link
+						to={`/drives/file/${file.id}`}
+						key={file.id}
+						className="@container mb-2 flex w-full max-w-28 shrink-0 cursor-pointer flex-col items-center gap-2 rounded-xl p-4 transition-all duration-75 hover:bg-slate-100"
+					>
+						<FileViewer
+							file={file}
+							className="aspect-square shrink-0 bg-white"
+							isCompact
+						/>
+						<p className="break-all text-gray-500 text-xs">{file.filename}</p>
+					</Link>
+				))
+			) : (
+				<div className="flex flex-col items-center">
+					<SpecificImage src="/mihoyo/no_files.webp" alt="No files" />
+					ファイルがまだありません
+				</div>
+			)}
 		</div>
 	);
 }
