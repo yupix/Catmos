b6759d4f3af5e59b21870782aee19f3a41fddab3
yupix <yupi0982@outlook.jp>
2025-03-11 22:24:23 +0900

    feat: 通知が無い場合に通知ルートでも通知が無いことを表示するように

--- app/routes/notifications.tsx
+++ app/routes/notifications.tsx
@@ -4,8 +4,8 @@ import { MainLayout } from '~/components/layouts/main-layout';
 import { Notification } from '~/components/notification';
 import { SidebarTrigger } from '~/components/shadcn/ui/sidebar';
 import { getUserSession } from '~/lib/auth/auth.server';
-import type { Route } from './+types';
 import { NotificationService } from '~/lib/notification.server';
+import type { Route } from './+types';
 
 export async function loader({ request }: Route.LoaderArgs) {
 	const user = await getUserSession(request);
@@ -15,7 +15,9 @@ export async function loader({ request }: Route.LoaderArgs) {
 		};
 	}
 
-	const notifications = await (new NotificationService(user.id).getNotifications());
+	const notifications = await new NotificationService(
+		user.id,
+	).getNotifications();
 
 	return {
 		notifications,
@@ -37,20 +39,31 @@ export default function Notifications() {
 			}
 		>
 			<AnimatePresence>
-				{notifications.map((notification) => (
-					<motion.div
-						className="my-6 border-b pb-6"
-						key={notification.id}
-						layout
-						initial={{ opacity: 0, y: -20, scale: 0.9 }}
-						animate={{ opacity: 1, y: 0, scale: 1 }}
-						exit={{ opacity: 0, y: -20, scale: 0.9 }}
-						transition={{ duration: 0.4, ease: 'easeOut' }}
-					>
-						{' '}
-						<Notification notification={notification} />
-					</motion.div>
-				))}
+				{notifications.length > 0 ? (
+					notifications.map((notification) => (
+						<motion.div
+							className="my-6 border-b pb-6"
+							key={notification.id}
+							layout
+							initial={{ opacity: 0, y: -20, scale: 0.9 }}
+							animate={{ opacity: 1, y: 0, scale: 1 }}
+							exit={{ opacity: 0, y: -20, scale: 0.9 }}
+							transition={{ duration: 0.4, ease: 'easeOut' }}
+						>
+							{' '}
+							<Notification notification={notification} />
+						</motion.div>
+					))
+				) : (
+					<div className="flex h-40 flex-col items-center justify-center">
+						<img
+							src="/mihoyo/not_notification.webp"
+							alt="通知はありません"
+							className="mb-4 h-30 w-30 rounded-xl"
+						/>
+						<p>通知はありません</p>
+					</div>
+				)}
 			</AnimatePresence>
 		</MainLayout>
 	);
