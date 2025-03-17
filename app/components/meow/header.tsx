8a0a6f2ee0334ab8d4701e13c31c88f237ad3d9c
yupix <yupi0982@outlook.jp>
2025-03-17 23:24:16 +0900

    feat: headerの文字サイズを固定、username部分の色を変更

--- app/components/meow/header.tsx
+++ app/components/meow/header.tsx
@@ -8,13 +8,18 @@ interface MeowHeaderProps {
 
 export const MeowHeader = ({ meow }: MeowHeaderProps) => {
 	return (
-		<div className="flex justify-between">
+		<div className="flex justify-between text-sm">
 			<HoverUserCard
 				user={meow.author}
 				className="shrink-999999 overflow-hidden overflow-ellipsis whitespace-nowrap"
 			>
+				<span className='font-semibold'>
 				{meow.author.displayName || meow.author.name}
+
+				</span>
+				<span className='text-slate-500'>
 				{meow.author.displayName ? `@${meow.author.name}` : null}
+				</span>
 			</HoverUserCard>
 
 			<div className="shrink-0">
